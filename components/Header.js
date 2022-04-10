8abd1ca615ff8b6114150b9843f7d67aae528861
Shaun Hartman <shaunh@teamsi.com>
2022-04-10 13:29:11 -0500

    NProgress

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,12 @@
 import Image from "next/image"
+import Link from "next/link"
 
 /* This example requires Tailwind CSS v2.0+ */
 const navigation = [
-  { name: 'Solutions', href: '#' },
-  { name: 'Pricing', href: '#' },
-  { name: 'Docs', href: '#' },
-  { name: 'Company', href: '#' },
+  { name: 'Listen', href: '/listen' },
+  { name: 'Shop Merch', href: '/merch' },
+  { name: 'Shop Vinyl', href: '/vinyl' },
+  { name: 'Sponsors', href: '/sponsors' },
 ]
 
 export default function Header() {
@@ -14,15 +15,15 @@ export default function Header() {
       <nav className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8" aria-label="Top">
         <div className="flex items-center justify-between w-full py-6 border-b border-indigo-500 lg:border-none">
           <div className="flex items-center">
-            <a href="#">
-              <span className="sr-only">Charlie Virgo Rocks</span>
+            <span className="sr-only">Charlie Virgo Rocks</span>
+            <Link passHref href="/">
               <Image src={'/charlie_virgo_logo_white.png'} width={150} height={90} alt="Charlie Virgo Logo" />
-            </a>
+            </Link>
             <div className="hidden ml-10 space-x-8 lg:block">
               {navigation.map((link) => (
-                <a key={link.name} href={link.href} className="text-base font-medium text-white hover:text-indigo-50">
-                  {link.name}
-                </a>
+                <Link passHref key={link.name} href={link.href} className="text-base font-medium text-white hover:text-indigo-50">
+                  <span className="text-white">{link.name}</span>
+                </Link>
               ))}
             </div>
           </div>
@@ -43,9 +44,9 @@ export default function Header() {
         </div>
         <div className="flex flex-wrap justify-center py-4 space-x-6 lg:hidden">
           {navigation.map((link) => (
-            <a key={link.name} href={link.href} className="text-base font-medium text-white hover:text-indigo-50">
+            <Link key={link.name} href={link.href} className="text-base font-medium text-white hover:text-indigo-50">
               {link.name}
-            </a>
+            </Link>
           ))}
         </div>
       </nav>
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import Page from '../components/Page'
+import Router from 'next/router'
+import 'nprogress/nprogress.css'
 import '../styles/globals.css'
+import NProgress from 'nprogress'
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 function MyApp({ Component, pageProps }) {
   return (
